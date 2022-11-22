ff933ee189c0960c74e6903bf06421f9972ab02a
razikhaNawaz <razikhafaheem02@gmail.com>
2022-11-22 15:53:37 +0530

    sixth commit

--- async.js
+++ async.js
@@ -3,34 +3,34 @@ console.log('person1:shows ticket');
 console.log('person2:shows ticket');
 const preMovie=async()=>{
     const promiseWifeBringing=new Promise((resolve,reject)=>{
-        reject('ticket',3000);
+        resolve('ticket',3000);
     });
-   /* const getPopcorn=new Promise((resolve,reject)=>resolve(`popcorn`));
+   const getPopcorn=new Promise((resolve,reject)=>resolve(`popcorn`));
 
     const getButter=new Promise((resolve,reject)=>resolve(`butter`));
     
     const getColddrinks=new Promise((resolve,reject)=>resolve(`colddrinks`));
-    /*let ticket=await promiseWifeBringing;
-    console.log('wife:i brought ticket');*/
+    let ticket=await promiseWifeBringing;
+    console.log('wife:i brought ticket');
 
-    //let popcorn=await getPopcorn;
-    //console.log('wife: i want popcorn');
+    let popcorn=await getPopcorn;
+    console.log('wife: i want popcorn');
 
-    //let butter=await getButter;
-    //console.log('wife:i want butter too');
-    //console.log('husband:gets butter')
+    let butter=await getButter;
+    console.log('wife:i want butter too');
+    console.log('husband:gets butter')
 
-    //let coldDrinks=await getColddrinks;*/
-    //console.log('wife:i want cold drinks');
-    //console.log('brought cold drinks')
+    let coldDrinks=await getColddrinks;
+    console.log('wife:i want cold drinks');
+    console.log('brought cold drinks')
     /*let msg=await Promise.all([getPopcorn,getButter,getColddrinks]); //promise.all all functions will be called together
     console.log(msg);*/
-    let ticket
+    /*let ticket
     try{
         ticket=await promiseWifeBringing;
     }catch(e){
         ticket='sad face';
-    }
+    }*/
      return ticket;
 }
 preMovie().then((m)=>console.log(`person3 shows ${m}`));
